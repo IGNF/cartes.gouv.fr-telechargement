ba158911c35fb53c4fea1e6d393ccf8675f74fea
mmohad <melodia.mohad@ign.fr>
2025-06-16 17:26:47 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  base: '/telechargement', // Ajoutez le base path ici
+  base: '/telechargement/', // Ajoutez le base path ici
   plugins: [TanStackRouterVite({}), react()],
 })
