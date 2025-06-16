57cf2db4593996da60a8c251446bbb9a817b7c7c
mmohad <melodia.mohad@ign.fr>
2025-06-16 14:11:58 +0200

    add base url /telechargement

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/telechargement/', // Ajoutez le base path ici
   plugins: [TanStackRouterVite({}), react()],
 })
