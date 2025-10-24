1b5bddb35f28a19c103d89bcec3628856d18e5c1
mmohad <melodia.mohad@ign.fr>
2025-10-24 16:34:56 +0200

    feat(footer): ajout footer minimal

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,87 +1,58 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
-import AppHeader from "../Components/AppHeader";
 import IgnfDsfrHeader from "../Components/Layout/IgnfDsfrHeader";
-import { Footer } from "@codegouvfr/react-dsfr/Footer";
+import AppFooter from "../Components/Layout/AppFooter";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
-
- 
   return (
     <>
-      {/* <AppHeader /> */}
-         <IgnfDsfrHeader
-         minimized={true}
+      <IgnfDsfrHeader
+        minimized={true}
+        menuItems={[
+          {
+            label: "Mon espace",
 
+            icon: "fr-icon-user-line",
 
-                menuItems={[
+            connectionMenu: true,
 
+            links: [
+              {
+                label: "Mon profil",
+                href: "/profil",
+                icon: "fr-icon-user-line",
+              },
 
-                    {
+              {
+                label: "Mes organisations",
+                href: "/organisations",
+                icon: "fr-icon-group-line",
+              },
+            ],
+          },
 
+          {
+            label: "Aide",
 
-                        label: "Mon espace",
+            icon: "fr-icon-question-line",
 
+            links: [
+              {
+                label: "Documentation",
+                href: "/docs",
+                icon: "fr-icon-book-2-line",
+              },
 
-                        icon: "fr-icon-user-line",
-
-
-                        connectionMenu: true,
-
-
-                        links: [
-
-
-                            { label: "Mon profil", href: "/profil", icon: "fr-icon-user-line" },
-
-
-                            { label: "Mes organisations", href: "/organisations", icon: "fr-icon-group-line" },
-
-
-                        ],
-
-
-                    },
-
-
-                    {
-
-
-                        label: "Aide",
-
-
-                        icon: "fr-icon-question-line",
-
-
-                        links: [
-
-
-                            { label: "Documentation", href: "/docs", icon: "fr-icon-book-2-line" },
-
-
-                            { label: "Contact", href: "/contact", icon: "fr-icon-mail-line" },
-
-
-                        ],
-
-
-                    },
-
-
-                ]}
-
-
-            />
+              { label: "Contact", href: "/contact", icon: "fr-icon-mail-line" },
+            ],
+          },
+        ]}
+      />
       <Outlet />
-      {/* <Footer
-        accessibility="partially compliant"
-        contentDescription="Le site officiel pour le téléchargement à la dalle des données raster IGN."
-        termsLinkProps={{ href: "#" }}
-        websiteMapLinkProps={{ href: "#" }}
-      /> */}
+      <AppFooter />
     </>
   );
 }
