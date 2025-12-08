070faaf2501a2dbecf9fa008e3c9d9855fb602b7
mmohad <melodia.mohad@ign.fr>
2025-12-08 14:21:22 +0100

    feat(filter): ajout d'un message pour l'absence de filtre

--- src/components/features/MenuCompenents/FilterComponents/Filter.jsx
+++ src/components/features/MenuCompenents/FilterComponents/Filter.jsx
@@ -1,23 +1,34 @@
+import React from "react";
 import { Button } from "@codegouvfr/react-dsfr/Button";
-import "./Filter.css"
+import { Alert } from "@codegouvfr/react-dsfr/Alert";
+import "./Filter.css";
 import FilterDate from "./FilterDate";
 
-const Filter = () => {
+const Filter = ({ onClose } = {}) => {
     return (
         <div className="filter">
-        <div className="filter-header">
-            <div className="filter-label">Filtrer</div>
-            <Button
-                onClick={function noRefCheck() { }}
-                priority="tertiary"
-            >
-                Réinitialiser
-            </Button>
-        </div>
-        <FilterDate />
-        </div>
-    )
 
-}
+            {onClose ? (
+                <Button
+                    iconId="fr-icon-arrow-left-line"
+                    iconPosition="left"
+                    priority="tertiary no outline"
+                    size="medium"
+                    onClick={onClose}>
+                    Retour
+                </Button>
+            ) : null}
+
+            <div className="filter-body" style={{ marginTop: 12 }}>
+                <strong>Aucun filtre disponible</strong>
+                <div style={{ marginTop: 6 }}>
+                    Pour cette donnée, aucun critère de filtrage n'est proposé pour l'instant.
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
 
 export default Filter;
