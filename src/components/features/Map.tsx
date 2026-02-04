e33c55e97a2a686127252691221b6b6eecaccdb2
mmohad <melodia.mohad@ign.fr>
2026-02-04 15:19:56 +0100

    add(react-ol): init

--- src/components/features/Map.tsx
+++ src/components/features/Map.tsx
@@ -1,53 +1,79 @@
-import React, { useEffect, useRef } from "react";
-import { useMap } from "../../hooks/maps/useMap";
-import { useDalleStore } from "../../hooks/store/useDalleStore";
+import React, { useEffect, useState } from "react";
+import { OSM } from "ol/source";
+import { Map, View, TileLayer } from "react-openlayers";
+import "react-openlayers/dist/index.css";
+
+
+import Gp from "geoportal-access-lib";
+import { LayerWMTS } from "geopf-extensions-openlayers";
 
 import "ol/ol.css";
 import "@gouvfr/dsfr/dist/dsfr.css";
 import "@gouvfr/dsfr/dist/utility/icons/icons.css";
 import "geopf-extensions-openlayers/css/Dsfr.css";
 import { getRouteApi } from "@tanstack/react-router";
-import { GeoJSON } from "ol/format";
-import SelectedOptions from "./MenuCompenents/SelectedOptions";
-import { set } from "ol/transform";
 const route = getRouteApi("/telechargement/$downloadUrl");
 
 const MapComponent = () => {
   const { downloadUrl } = route.useParams();
+  const [isConfigLoaded, setIsConfigLoading] =useState(false)
 
-  const addProduit = useDalleStore((state) => state.addProduit);
-  const isProduitSelected = useDalleStore((state) => state.isProduitSelected);
-  const removeDalle = useDalleStore((state) => state.removeProduit);
-  const addProduitLayer = useDalleStore((state) => state.addProduitLayer);
-  const addChantierLayer = useDalleStore((state) => state.addChantierLayer);
-  const setIsMetadata = useDalleStore((state) => state.setIsMetadata);
+  const getConfig = async () => {
+    const config = new Gp.Services.Config({
+      customConfigFile:
+        "https://raw.githubusercontent.com/IGNF/geoportal-configuration/new-url/dist/fullConfig.json",
+      onSuccess: ()=> {setIsConfigLoading(true)},
+      onFailure: (e) => console.error(e),
+    });
+    await config.call();
+  };
 
-  const mapContainerRef = useRef<HTMLDivElement>(null);
-
-useMap(
-    mapContainerRef,
-    downloadUrl,
-    addProduit,
-    isProduitSelected,
-    removeDalle,
-    addProduitLayer,
-    addChantierLayer,
-    setIsMetadata
-  );
-
+  useEffect(() => {
+    getConfig();
+  },[]);
+if (isConfigLoaded) {
+  
 
   return (
     <>
+    
       <div
-        ref={mapContainerRef}
         id="map"
         className="map-container fr-col-8"
         style={{ height: "80vh", width: "100%" }}
-      ></div>
-
-      <SelectedOptions />
+      >
+        <Map controls={[]}>
+          <TileLayer
+            source={
+              new LayerWMTS({
+                layer: "GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2",
+              })
+            }
+          />
+          <View
+            center={[288074.8449901076, 6247982.515792289]}
+            zoom={6}
+            maxZoom={16}
+          />
+        </Map>
+      </div>
     </>
-  );
+  );}
+  else{
+    
+  return(
+        <>
+    
+      <div
+        id="map"
+        className="map-container fr-col-8"
+        style={{ height: "80vh", width: "100%" }}
+      >
+        
+      </div>
+    </>
+
+  )}
 };
 
 export default MapComponent;
